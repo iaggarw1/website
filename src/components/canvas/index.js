ed03bbe71d9432ad261ab2cd259a3ee2238da993
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2024-02-14 01:45:08 -0800

    Modified gitignore to add packages for remote developement on laptop. Completed Hero section with model

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./Earth.jsx";
 import BallCanvas from "./Ball.jsx";
-import ComputersCanvas from "./Computers.jsx";
+import ComputersCanvas from "./ComputersCanvas.jsx";
 import StarsCanvas from "./Stars.jsx";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
