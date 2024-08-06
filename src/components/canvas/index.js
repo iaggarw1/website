697c3d39813fa9035cc79b742530478bbc224f50
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2024-08-05 20:35:58 -0700

    reset branch

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth.jsx";
-import BallCanvas from "./Ball.jsx";
-import ComputersCanvas from "./ComputersCanvas.jsx";
-import StarsCanvas from "./Stars.jsx";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
