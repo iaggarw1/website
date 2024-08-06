19c11195bdd9432e63ddf475150b6bc725175f1a
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2024-08-06 03:14:00 -0700

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
 import React, { useState } from 'react';
-import ContactForm from './components/ContactForm';
 
 function App() {
-  const [currentStep, setCurrentStep] = useState(0);
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    alert('Form submitted!');
-  };
-
   return (
-    <div className="App">
-      <ContactForm
-        currentStep={currentStep}
-        inputValue={inputValue}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        setCurrentStep={setCurrentStep}
-        setInputValue={setInputValue}
-      />
+    <div>
     </div>
   );
 }
